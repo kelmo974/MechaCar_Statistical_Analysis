4c015f46e4375a3b33158db455c8e8ba39349656
Kellen <kellen.finn@gmail.com>
2022-12-30 15:49:08 -0500

    pushign again

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -16,3 +16,13 @@ coil <- read.csv("Suspension_Coil.csv")
 total_summary <- coil %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), standard_deviation = sd(PSI))
 # similar summary analysis, but by lot grouping
 lot_summary <- coil %>%  group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), standard_deviation = sd(PSI))
+
+
+# perform t.test on suspension coils 
+t.test(coil$PSI, mu = 1500, alternative = "two.sided")
+
+
+# perform t.tests on three different lot # categories/groups
+t.test(subset(coil, Manufacturing_Lot == "Lot1")$PSI, mu = 1500, alternative = "two.sided")
+t.test(subset(coil, Manufacturing_Lot == "Lot2")$PSI, mu = 1500, alternative = "two.sided")
+t.test(subset(coil, Manufacturing_Lot == "Lot3")$PSI, mu = 1500, alternative = "two.sided")
