3af76596471da09396c54b57447208f59cc65f58
Kellen <kellen.finn@gmail.com>
2022-12-30 15:32:46 -0500

    updating images and R script

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -10,3 +10,9 @@ summary(car_mpg)
 
 # import second CSV
 coil <- read.csv("Suspension_Coil.csv")
+
+
+# perform summary analysis on suspension csv
+total_summary <- coil %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), standard_deviation = sd(PSI))
+# similar summary analysis, but by lot grouping
+lot_summary <- coil %>%  group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), standard_deviation = sd(PSI))
